d1ccac0b9043768edd4bf82cdce9a784eca7c834
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2023-09-27 22:34:38 -0300

    feat: add base API routes with mocked responses

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,49 @@
-use actix_web;
+use actix_web::{self, get, post, web, App, HttpResponse, HttpServer, Responder};
+use serde::Deserialize;
 
 mod models;
 
 use crate::models::person::Person;
 
-#[actix_web::get("/health")]
-async fn healthcheck() -> impl actix_web::Responder {
-    actix_web::HttpResponse::Ok()
-        .content_type("text/html")
-        .body("Server is alive")
+#[derive(Deserialize)]
+struct FetchPersonByTermQueryParams {
+    t: String,
 }
 
-#[actix_web::post("/pessoas")]
-async fn store_person(new_person: actix_web::web::Json<Person>) -> impl actix_web::Responder {
-    actix_web::HttpResponse::Created()
+#[post("/pessoas")]
+async fn store_person(_new_person: web::Json<Person>) -> impl Responder {
+    HttpResponse::Created()
+        .content_type("text/html")
+        .body("Person stored")
+}
+
+#[get("/pessoas/{id}")]
+async fn fetch_person_by_id(path: web::Path<(String,)>) -> impl Responder {
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(format!("Person by id: {}", path.0))
+}
+
+#[get("/pessoas")]
+async fn fetch_person_by_term(query: web::Query<FetchPersonByTermQueryParams>) -> impl Responder {
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(format!("Persons by term: {}", query.t))
+}
+
+#[get("/contagem-pessoas")]
+async fn count_persons() -> impl Responder {
+    HttpResponse::Ok().content_type("text/html").body("11")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    actix_web::HttpServer::new(|| {
-        actix_web::App::new()
-            .service(healthcheck)
+    HttpServer::new(|| {
+        App::new()
             .service(store_person)
+            .service(fetch_person_by_id)
+            .service(fetch_person_by_term)
+            .service(count_persons)
     })
     .bind(("127.0.0.1", 5555))?
     .run()
