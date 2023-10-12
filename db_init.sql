bbc142d10a2e36492d0d526b6b73a3108ea427b8
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2023-10-12 17:11:30 -0300

    feat: add constraints to persons table columns

--- db_init.sql
+++ db_init.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS persons (
     id       UUID PRIMARY KEY NOT NULL,
-    nickname TEXT NOT NULL,
-    name     TEXT NOT NULL,
-    birth    TEXT NOT NULL,
+    nickname VARCHAR(32) UNIQUE NOT NULL,
+    name     VARCHAR(100) NOT NULL,
+    birth    VARCHAR(10) NOT NULL,
     stack    TEXT
 );
