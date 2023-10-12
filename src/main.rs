7cc299b645204ee3899eca8e0ab00a8d0034a969
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2023-10-12 18:40:12 -0300

    feat: add postgres pool and share it on app state

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,36 @@
-use actix_web::{self, App, HttpServer};
+use std::env;
+
+use actix_web::{self, web::Data, App, HttpServer};
+use dotenv::dotenv;
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 
 mod handlers;
 mod models;
 
-pub struct AppState {}
+pub struct AppState {
+    pub database_client: Pool<Postgres>,
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL env must be set");
+    let postgres_pool = PgPoolOptions::new()
+        .max_connections(10)
+        .connect(&database_url)
+        .await
+        .expect("Error to connect on Postgres database");
+
+    println!("🚀 Server started successfully");
+
+    HttpServer::new(move || {
+        let app_state = AppState {
+            database_client: postgres_pool.clone(),
+        };
+
         App::new()
+            .app_data(Data::new(app_state))
             .service(handlers::store_person)
             .service(handlers::fetch_person_by_id)
             .service(handlers::fetch_person_by_term)
