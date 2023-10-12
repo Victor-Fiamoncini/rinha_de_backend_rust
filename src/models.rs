e53e2a9d0b3b1f8215972b99eb5203aeb061a15e
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2023-10-12 18:40:47 -0300

    feat: add NewPerson struct to handle http request payloads

--- src/models.rs
+++ src/models.rs
@@ -1,8 +1,17 @@
 use serde::{Deserialize, Serialize};
+use sqlx::FromRow;
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
-pub struct Person {
-    pub id: Option<String>,
+pub struct NewPerson {
+    pub nickname: String,
+    pub name: String,
+    pub birth: String,
+    pub stack: Option<Vec<String>>,
+}
+
+#[derive(Clone, Debug, Deserialize, FromRow, Serialize)]
+pub struct StoredPerson {
+    pub id: String,
     pub nickname: String,
     pub name: String,
     pub birth: String,
