1e79ea7509f5f75f536d3caa0a03397805f5ad20
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2023-10-12 18:41:46 -0300

    feat: add base store_person validations

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,35 +1,57 @@
-use actix_web::{get, post, web, HttpResponse, Responder};
+use actix_web::{
+    get, post,
+    web::{self},
+    HttpResponse, Responder,
+};
+use regex::Regex;
 use serde::Deserialize;
 
-use crate::models::person::Person;
-
-#[derive(Deserialize)]
-pub struct FetchPersonByTermQueryParams {
-    pub t: String,
-}
+use crate::models::NewPerson;
 
 #[post("/pessoas")]
-async fn store_person(_new_person: web::Json<Person>) -> impl Responder {
+async fn store_person(new_person: web::Json<NewPerson>) -> impl Responder {
+    if new_person.nickname.is_empty() || new_person.nickname.len() > 32 {
+        return HttpResponse::BadRequest();
+    }
+
+    if new_person.name.is_empty() || new_person.name.len() > 100 {
+        return HttpResponse::BadRequest();
+    }
+
+    let dateformat_regex = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
+
+    if new_person.birth.is_empty() || !dateformat_regex.is_match(&new_person.birth) {
+        return HttpResponse::BadRequest();
+    }
+
+    match &new_person.stack {
+        Some(stack) => {
+            if stack.iter().any(|tech| tech.is_empty() || tech.len() > 32) {
+                return HttpResponse::BadRequest();
+            }
+        }
+        None => {}
+    }
+
     HttpResponse::Created()
-        .content_type("text/html")
-        .body("Person stored")
 }
 
 #[get("/pessoas/{id}")]
 async fn fetch_person_by_id(path: web::Path<(String,)>) -> impl Responder {
     HttpResponse::Ok()
-        .content_type("text/html")
-        .body(format!("Person by id: {}", path.0))
+}
+
+#[derive(Deserialize)]
+struct FetchPersonByTermParams {
+    pub t: String,
 }
 
 #[get("/pessoas")]
-async fn fetch_person_by_term(query: web::Query<FetchPersonByTermQueryParams>) -> impl Responder {
+async fn fetch_person_by_term(query: web::Query<FetchPersonByTermParams>) -> impl Responder {
     HttpResponse::Ok()
-        .content_type("text/html")
-        .body(format!("Persons by term: {}", query.t))
 }
 
 #[get("/contagem-pessoas")]
 async fn count_persons() -> impl Responder {
-    HttpResponse::Ok().content_type("text/html").body("11")
+    HttpResponse::Ok()
 }
