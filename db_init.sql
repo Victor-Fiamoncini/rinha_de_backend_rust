53a8b7d1dfebc0168309ad4b79c93eac8e71d745
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2023-10-17 22:31:31 -0300

    refactor: make stack column an array of strings

--- db_init.sql
+++ db_init.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS persons (
-    id       UUID PRIMARY KEY NOT NULL,
+    id UUID PRIMARY KEY NOT NULL,
     nickname VARCHAR(32) UNIQUE NOT NULL,
-    name     VARCHAR(100) NOT NULL,
-    birth    VARCHAR(10) NOT NULL,
-    stack    TEXT
+    name VARCHAR(100) NOT NULL,
+    birth VARCHAR(10) NOT NULL,
+    stack VARCHAR(32)[]
 );
