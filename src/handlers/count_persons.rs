d2f5eda76c9829a29922f6e1f5750523ecc82e33
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2023-10-28 17:20:35 -0300

    feat: add count_persons handler integration with database

--- src/handlers/count_persons.rs
+++ src/handlers/count_persons.rs
@@ -1,6 +1,15 @@
-use actix_web::{get, HttpResponse, Responder};
+use actix_web::{get, http::header::ContentType, web::Data, HttpResponse, Responder};
+
+use crate::{models::PersonsCount, AppState};
 
 #[get("/contagem-pessoas")]
-async fn count_persons() -> impl Responder {
+async fn count_persons(state: Data<AppState>) -> impl Responder {
+    let persons_count = sqlx::query_as::<_, PersonsCount>("SELECT COUNT(*) AS count FROM persons")
+        .fetch_one(&state.database_pool)
+        .await
+        .unwrap_or_else(|_| PersonsCount { count: 0 });
+
     HttpResponse::Ok()
+        .content_type(ContentType::json())
+        .body(persons_count.count.to_string())
 }
