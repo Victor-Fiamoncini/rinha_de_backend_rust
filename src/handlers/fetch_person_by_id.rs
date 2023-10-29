cdcaa281df397afdd4463d36e8f8d5084671faac
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2023-10-29 04:09:02 -0300

    feat: add fetch_person_by_id handler integration with database

--- src/handlers/fetch_person_by_id.rs
+++ src/handlers/fetch_person_by_id.rs
@@ -1,6 +1,23 @@
-use actix_web::{get, web, HttpResponse, Responder};
+use actix_web::{
+    get,
+    web::{Data, Path},
+    HttpResponse, Responder,
+};
+use uuid::Uuid;
+
+use crate::{models::StoredPerson, AppState};
 
 #[get("/pessoas/{id}")]
-async fn fetch_person_by_id(_path: web::Path<(String,)>) -> impl Responder {
-    HttpResponse::Ok()
+async fn fetch_person_by_id(state: Data<AppState>, id: Path<Uuid>) -> impl Responder {
+    let person_by_id_result = sqlx::query_as::<_, StoredPerson>(
+        "SELECT p.id, p.nickname, p.name, p.birth, p.stack FROM persons p WHERE p.id = $1",
+    )
+    .bind(id.into_inner())
+    .fetch_one(&state.database_pool)
+    .await;
+
+    match person_by_id_result {
+        Ok(person) => HttpResponse::Ok().json(person),
+        Err(_) => HttpResponse::NotFound().json(()),
+    }
 }
