98fb27ce07f843e838cfacd1d6a3406aecb421bc
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2023-10-29 21:23:26 -0300

    feat: add search_terms column and gist index on persons table

--- db_init.sql
+++ db_init.sql
@@ -1,7 +1,18 @@
+CREATE EXTENSION pg_trgm;
+
+CREATE OR REPLACE FUNCTION generate_search_terms(nickname VARCHAR, name VARCHAR, stack VARCHAR[]) RETURNS TEXT AS $$
+BEGIN
+    RETURN (nickname || ' ' || name || ' ' || ARRAY_TO_STRING(stack, ' '));
+END;
+$$ LANGUAGE plpgsql IMMUTABLE;
+
 CREATE TABLE IF NOT EXISTS persons (
     id UUID PRIMARY KEY NOT NULL,
     nickname VARCHAR(32) UNIQUE NOT NULL,
     name VARCHAR(100) NOT NULL,
     birth VARCHAR(10) NOT NULL,
-    stack VARCHAR(32)[]
+    stack VARCHAR(32)[],
+    search_terms TEXT GENERATED ALWAYS AS (generate_search_terms(nickname, name, stack)) STORED
 );
+
+CREATE INDEX persons_search_terms_idx ON persons USING GIST (search_terms gist_trgm_ops);
