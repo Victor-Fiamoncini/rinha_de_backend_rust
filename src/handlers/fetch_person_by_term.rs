a61c7b7f8f206d63d86639ebb28b1ab5c6424930
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2023-10-29 21:22:29 -0300

    feat: validate search query param on fetch_person_by_term handler

--- src/handlers/fetch_person_by_term.rs
+++ src/handlers/fetch_person_by_term.rs
@@ -1,12 +1,32 @@
-use actix_web::{get, web, HttpResponse, Responder};
+use actix_web::{
+    get,
+    web::{self, Data},
+    HttpResponse, Responder,
+};
 use serde::Deserialize;
 
+use crate::AppState;
+
 #[derive(Deserialize)]
-struct FetchPersonByTermParams {
-    pub _t: String,
+struct FetchPersonByTermQueryParams {
+    pub t: Option<String>,
 }
 
 #[get("/pessoas")]
-async fn fetch_person_by_term(_query: web::Query<FetchPersonByTermParams>) -> impl Responder {
+async fn fetch_person_by_term(
+    state: Data<AppState>,
+    query: web::Query<FetchPersonByTermQueryParams>,
+) -> impl Responder {
+    match &query.t {
+        Some(term) => {
+            if term.is_empty() || term.len() == 0 {
+                return HttpResponse::BadRequest();
+            }
+        }
+        None => {
+            return HttpResponse::BadRequest();
+        }
+    }
+
     HttpResponse::Ok()
 }
