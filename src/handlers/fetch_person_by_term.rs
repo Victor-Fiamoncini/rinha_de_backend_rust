b813638f9a4d5cbe3e8a6c0c1d4d90c8ccd0b0fb
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2023-10-31 21:27:25 -0300

    feat: add fetch_person_by_term handler integration with database

--- src/handlers/fetch_person_by_term.rs
+++ src/handlers/fetch_person_by_term.rs
@@ -5,11 +5,12 @@ use actix_web::{
 };
 use serde::Deserialize;
 
-use crate::AppState;
+use crate::{models::StoredPerson, AppState};
 
 #[derive(Deserialize)]
 struct FetchPersonByTermQueryParams {
-    pub t: Option<String>,
+    #[serde(rename(deserialize = "t"))]
+    pub search: Option<String>,
 }
 
 #[get("/pessoas")]
@@ -17,16 +18,31 @@ async fn fetch_person_by_term(
     state: Data<AppState>,
     query: web::Query<FetchPersonByTermQueryParams>,
 ) -> impl Responder {
-    match &query.t {
-        Some(term) => {
+    let stringfied_query: String;
+
+    match &query.search {
+        Some(ref term) => {
             if term.is_empty() || term.len() == 0 {
-                return HttpResponse::BadRequest();
+                return HttpResponse::BadRequest().json(());
             }
+
+            stringfied_query = term.to_string();
         }
         None => {
-            return HttpResponse::BadRequest();
+            return HttpResponse::BadRequest().json(());
         }
     }
 
-    HttpResponse::Ok()
+    let persons_by_term = sqlx::query_as!(
+        StoredPerson,
+        "SELECT p.id, p.nickname, p.name, p.birth, p.stack FROM persons p WHERE p.search_terms ILIKE $1 LIMIT 50",
+        format!("%{stringfied_query}%")
+    )
+    .fetch_all(&state.database_pool)
+    .await;
+
+    match persons_by_term {
+        Ok(persons) => HttpResponse::Ok().json(persons),
+        Err(_) => HttpResponse::NotFound().json(()),
+    }
 }
